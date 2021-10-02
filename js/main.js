1befd2ce1a90de6ac7548c5ac2c754d45aadd7eb
Kashish <67566408+kashish-d@users.noreply.github.com>
2021-10-02 07:56:56 +0530

    Revert "Fixed contact page "

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-var form = document.forms["contactForm"];
-	form.addEventListener('submit',contact_submit,false);
-
-	function contact_submit(e) {
-		// Stop Form From Submitting
-		e.preventDefault();
-
-		// Set Initial Variables
-		var target = e.target || e.srcElement;
-		var to = '7sakuraixiao@gmail.com';
-		var uri = 'mailto:' + to;
-		var body = '';
-
-		// Set Form Values to Variables
-		var name = target.elements['name'].value;
-		var subject = target.elements['subject'].value;
-		var message = target.elements['message'].value;
-
-		// Build Body / Message with all Input Fields
-		body += message + "\r\n\r\n";
-
-		// Build final Mailto URI
-		uri += '?subject=' + encodeURIComponent(subject);
-		uri += '&body=' + encodeURIComponent(body);
-
-		// Open Mailto in New Window / Tab
-		window.open(uri);
-	}
-
-
 (function($) {
 
 	"use strict";
